4824cc93a801a9c6b6ac1558672941934e7e7ef2
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-11-08 18:14:05 +0000

    - creater heart beat app - create html file - create css file

---
+++ src/pages/heartbeat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./heatbeat.css";
+
+const Heartbeat = () => {
+  return (
+    <div id="conatiner">
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
+  );
+};
+
+export default Heartbeat;
