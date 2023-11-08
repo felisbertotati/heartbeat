d7fb8bc33ac420c9c2d3fea85b6c79db01be53e7
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-11-08 18:43:44 +0000

    - added sprinkles to the heart

--- src/pages/heartbeat.js
+++ src/pages/heartbeat.js
@@ -2,8 +2,24 @@ import React from "react";
 import "../pages/heartbeat";
 
 const Heartbeat = () => {
+  // Generating a range (e.g., 0 to 19) for 20 sprinkles
+  const sprinkleElements = [...Array(30).keys()].map((i) => (
+    <div
+      key={i}
+      className={`sprinkle`}
+      style={{
+        // Randomize the position of the sprinkle
+        top: `${Math.random() * 100}%`,
+        left: `${Math.random() * 100}%`,
+        // Apply random animation delays and durations to each sprinkle
+        animationDelay: `${Math.random() * 2}s`,
+        animationDuration: `${2 + Math.random() * 3}s`,
+      }}
+    />
+  ));
   return (
     <div id="container">
+      {sprinkleElements}
       <div className="heart-part left"></div>
       <div className="heart-part right"></div>
     </div>
